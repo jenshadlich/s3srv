5b14f7ee180be0df06213ccbd7a8c2082fe71c10
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-24 23:08:20 +0200

    simple create bucket implemented

--- src/main/java/de/jeha/s3srv/operations/buckets/CreateBucket.java
+++ src/main/java/de/jeha/s3srv/operations/buckets/CreateBucket.java
@@ -1,13 +1,19 @@
 package de.jeha.s3srv.operations.buckets;
 
 import com.codahale.metrics.annotation.Timed;
-import de.jeha.s3srv.storage.S3Bucket;
 import de.jeha.s3srv.storage.StorageBackend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author jenshadlich@googlemail.com
@@ -26,12 +32,16 @@ public class CreateBucket {
     @Path("/{bucket}/")
     @Produces(MediaType.APPLICATION_XML)
     @Timed
-    public void createBucket(@PathParam("bucket") String bucket) {
+    public Response createBucket(@Context HttpHeaders headers,
+                                 @Context HttpServletResponse response,
+                                 @PathParam("bucket") String bucket) {
         LOG.info("createBucket {}", bucket);
 
         storageBackend.createBucket(bucket);
 
-        throw new UnsupportedOperationException("Not implemented yet!");
+        response.addHeader("Location", "/" + bucket);
+
+        return Response.ok().build();
     }
 
 }
