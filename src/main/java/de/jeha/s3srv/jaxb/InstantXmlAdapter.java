a92a095ea3716cb1e25cc14308c3b7cdb72ec8a3
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-27 22:46:26 +0200

    add XmlAdapter for java.time.Instant

---
+++ src/main/java/de/jeha/s3srv/jaxb/InstantXmlAdapter.java
@@ -0,0 +1,24 @@
+package de.jeha.s3srv.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class InstantXmlAdapter extends XmlAdapter<String, Instant> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_INSTANT;
+
+    @Override
+    public Instant unmarshal(String value) throws Exception {
+        return value != null ? formatter.parse(value, Instant::from) : null;
+    }
+
+    @Override
+    public String marshal(Instant value) throws Exception {
+        return value != null ? formatter.format(value) : null;
+    }
+
+}
