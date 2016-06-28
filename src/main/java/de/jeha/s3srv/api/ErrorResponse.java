42d17f32471c02c8973166e73bff046fb067e951
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-28 21:09:30 +0200

    add error response

---
+++ src/main/java/de/jeha/s3srv/api/ErrorResponse.java
@@ -0,0 +1,50 @@
+package de.jeha.s3srv.api;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+@XmlRootElement(name = "Error")
+public class ErrorResponse {
+
+    @XmlElement(name = "Code")
+    private String code;
+
+    @XmlElement(name = "Message")
+    private String message;
+
+    @XmlElement(name = "Resource")
+    private String resource;
+
+    @XmlElement(name = "RequestId")
+    private String requestId;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(String code, String message, String resource, String requestId) {
+        this.code = code;
+        this.message = message;
+        this.resource = resource;
+        this.requestId = requestId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+}
