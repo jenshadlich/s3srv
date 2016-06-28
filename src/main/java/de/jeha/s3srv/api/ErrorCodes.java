11073b5157388752f52431e843d9a85fcd31c12e
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-28 21:10:41 +0200

    start collecting error codes

---
+++ src/main/java/de/jeha/s3srv/api/ErrorCodes.java
@@ -0,0 +1,41 @@
+package de.jeha.s3srv.api;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public enum ErrorCodes {
+
+    BUCKET_ALREADY_EXISTS(
+            "BucketAlreadyExists",
+            "The requested bucket name is not available. The bucket namespace is shared by all users of the system. Please select a different name and try again.",
+            409
+    ),
+    INVALID_BUCKET_NAME(
+            "InvalidBucketName",
+            "The specified bucket is not valid.",
+            400
+    );
+
+    private final String code;
+    private final String description;
+    private final int statusCode;
+
+    ErrorCodes(String code, String description, int statusCode) {
+        this.code = code;
+        this.description = description;
+        this.statusCode = statusCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+}
