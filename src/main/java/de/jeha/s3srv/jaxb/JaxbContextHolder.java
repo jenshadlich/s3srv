b265cc36d98deed9a7139b6c3f75489a3fb53944
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-29 20:12:00 +0200

    add ListBucketResult

--- src/main/java/de/jeha/s3srv/jaxb/JaxbContextHolder.java
+++ src/main/java/de/jeha/s3srv/jaxb/JaxbContextHolder.java
@@ -2,6 +2,7 @@ package de.jeha.s3srv.jaxb;
 
 import de.jeha.s3srv.api.ErrorResponse;
 import de.jeha.s3srv.api.ListAllMyBucketsResponse;
+import de.jeha.s3srv.api.ListBucketResult;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -17,7 +18,8 @@ class JaxbContextHolder {
         try {
             JAXB_CONTEXT = JAXBContext.newInstance(
                     ErrorResponse.class,
-                    ListAllMyBucketsResponse.class
+                    ListAllMyBucketsResponse.class,
+                    ListBucketResult.class
             );
         } catch (JAXBException e) {
             throw new RuntimeException("Unable to initialize JAXB Context", e);
