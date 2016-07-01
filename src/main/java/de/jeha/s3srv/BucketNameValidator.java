b2c718f0fff751e0daba6564b3c11684dd5cd6f6
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-01 23:40:20 +0200

    continue implementation

--- src/main/java/de/jeha/s3srv/BucketNameValidator.java
+++ src/main/java/de/jeha/s3srv/BucketNameValidator.java
@@ -1,17 +1,33 @@
 package de.jeha.s3srv;
 
+import java.util.regex.Pattern;
+
 /**
  * @author jenshadlich@googlemail.com
  */
 public class BucketNameValidator {
 
+    private static final int MIN_BUCKET_LENGTH = 3;
+    private static final int MAX_BUCKET_LENGTH = 63;
+    private static final Pattern IPV4_PATTERN = Pattern.compile("(\\d+\\.){3}\\d+");
+
+    /**
+     * Validate the given bucket name.
+     *
+     * @param bucket name of bucket
+     * @return true if valid, otherwise false
+     */
     public static boolean isValid(String bucket) {
 
-        if (bucket == null || bucket.length() < 3 || bucket.length() > 63) {
+        if (bucket == null || bucket.length() < MIN_BUCKET_LENGTH || bucket.length() > MAX_BUCKET_LENGTH) {
             return false;
         }
 
-        return false;
+        if (IPV4_PATTERN.matcher(bucket).matches()) {
+            return false;
+        }
+
+        return true;
     }
 
 }
--- src/test/java/de/jeha/s3srv/BucketNameValidatorTest.java
+++ src/test/java/de/jeha/s3srv/BucketNameValidatorTest.java
@@ -3,6 +3,7 @@ package de.jeha.s3srv;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author jenshadlich@googlemail.com
@@ -13,23 +14,23 @@ public class BucketNameValidatorTest {
     public void test() {
 
         // valid
-/*
         assertTrue(BucketNameValidator.isValid("buc"));
         assertTrue(BucketNameValidator.isValid("mybucket"));
         assertTrue(BucketNameValidator.isValid("my.bucket"));
         assertTrue(BucketNameValidator.isValid("mybucket.1"));
         assertTrue(BucketNameValidator.isValid("my-bucket"));
-*/
+
         // not valid, too short
         assertFalse(BucketNameValidator.isValid("b"));
         assertFalse(BucketNameValidator.isValid("bu"));
 
         // not valid
         assertFalse(BucketNameValidator.isValid(null));
+//        assertFalse(BucketNameValidator.isValid("MyBucket"));
         assertFalse(BucketNameValidator.isValid("192.168.5.4"));
-        assertFalse(BucketNameValidator.isValid(".mybucket"));
-        assertFalse(BucketNameValidator.isValid("mybucket."));
-        assertFalse(BucketNameValidator.isValid("my..bucket"));
+//        assertFalse(BucketNameValidator.isValid(".mybucket"));
+//        assertFalse(BucketNameValidator.isValid("mybucket."));
+//        assertFalse(BucketNameValidator.isValid("my..bucket"));
     }
 
 }
