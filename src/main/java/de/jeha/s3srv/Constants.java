85d62841c6125f53f289bac5927b10e54ca06730
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-02 00:03:02 +0200

    refactor

---
+++ src/main/java/de/jeha/s3srv/Constants.java
@@ -0,0 +1,10 @@
+package de.jeha.s3srv;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Constants {
+
+    public static final String XML_NAMESPACE = "http://s3.amazonaws.com/doc/2006-03-01/";
+
+}
--- src/main/java/de/jeha/s3srv/api/package-info.java
+++ src/main/java/de/jeha/s3srv/api/package-info.java
@@ -1,6 +1,7 @@
-@XmlSchema(namespace = "http://s3.amazonaws.com/doc/2006-03-01",
-           elementFormDefault = XmlNsForm.QUALIFIED)
+@XmlSchema(namespace = Constants.XML_NAMESPACE, elementFormDefault = XmlNsForm.QUALIFIED)
 package de.jeha.s3srv.api;
+
+import de.jeha.s3srv.Constants;
 
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
