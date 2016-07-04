d85dd0e67c3574f2e731ec6d269cb53c6860e124
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-04 22:09:11 +0200

    prepare ACL implementation

---
+++ src/main/java/de/jeha/s3srv/common/acl/ACLPermissions.java
@@ -0,0 +1,12 @@
+package de.jeha.s3srv.common.acl;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public enum ACLPermissions {
+    READ,
+    WRITE,
+    READ_ACP,
+    WRITE_ACP,
+    FULL_CONTROL;
+}
---
+++ src/main/java/de/jeha/s3srv/common/acl/CannedACLs.java
@@ -0,0 +1,18 @@
+package de.jeha.s3srv.common.acl;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public enum CannedACLs {
+
+    PRIVATE("private"),
+    PUBLIC_READ("public-read"),
+    PUBLIC_READ_WRITE("public-read-write"),
+    AUTHENTICATED_READ("authenticated-read");
+
+    private final String value;
+
+    CannedACLs(String value) {
+        this.value = value;
+    }
+}
