0989b614ec48d483d6ea4d87eafc2c3e63fa46d5
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-06 22:13:25 +0200

    check access key and signature on bucket creation

--- src/main/java/de/jeha/s3srv/operations/buckets/CreateBucket.java
+++ src/main/java/de/jeha/s3srv/operations/buckets/CreateBucket.java
@@ -3,18 +3,20 @@ package de.jeha.s3srv.operations.buckets;
 import com.codahale.metrics.annotation.Timed;
 import de.jeha.s3srv.common.BucketNameValidator;
 import de.jeha.s3srv.common.errors.ErrorCodes;
+import de.jeha.s3srv.common.security.AuthorizationContext;
+import de.jeha.s3srv.common.security.AuthorizationUtils;
+import de.jeha.s3srv.common.security.Credentials;
 import de.jeha.s3srv.operations.AbstractOperation;
 import de.jeha.s3srv.storage.StorageBackend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ValidationException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 /**
@@ -32,26 +34,28 @@ public class CreateBucket extends AbstractOperation {
     @PUT
     @Path("/{bucket}/")
     @Timed
-    public Response createBucket(@Context HttpHeaders headers,
-                                 @Context HttpServletResponse response,
+    public Response createBucket(@Context HttpServletRequest request,
                                  @PathParam("bucket") String bucket) {
-        LOG.info("createBucket {}", bucket);
+        LOG.info("createBucket '{}'", bucket);
+        final String resource = "/" + bucket + "/";
 
-        final String date = headers.getHeaderString("Date");
-        final String authorization = headers.getHeaderString("Authorization");
-
-        LOG.info("Date: {}", date);
-        LOG.info("Authorization: {}", authorization);
+        AuthorizationContext authorizationContext = checkAuthorization(request, resource);
+        if (!authorizationContext.isAccessKeyValid()) {
+            return createErrorResponse(ErrorCodes.INVALID_ACCESS_KEY_ID, resource, null);
+        }
+        if (!authorizationContext.isSignatureValid()) {
+            return createErrorResponse(ErrorCodes.SIGNATURE_DOES_NOT_MATCH, resource, null);
+        }
 
         try {
             BucketNameValidator.isValid(bucket);
         } catch (ValidationException e) {
             LOG.info("Invalid bucket name: {}", e.getMessage());
-            return createErrorResponse(ErrorCodes.INVALID_BUCKET_NAME, "/" + bucket, null);
+            return createErrorResponse(ErrorCodes.INVALID_BUCKET_NAME, resource, null);
         }
 
         if (getStorageBackend().existsBucket(bucket)) {
-            return createErrorResponse(ErrorCodes.BUCKET_ALREADY_EXISTS, "/" + bucket, null);
+            return createErrorResponse(ErrorCodes.BUCKET_ALREADY_EXISTS, resource, null);
         } else {
             getStorageBackend().createBucket(bucket);
 
@@ -61,6 +65,29 @@ public class CreateBucket extends AbstractOperation {
                     .header("Connection", "close")
                     .build();
         }
+    }
+
+    private AuthorizationContext checkAuthorization(HttpServletRequest request, String resource) {
+        final String date = request.getHeader("Date");
+        final String authorization = request.getHeader("Authorization");
+        final String contentMD5 = request.getHeader("Content-MD5");
+        final String contentType = request.getHeader("Content-Type");
+
+        LOG.info("Date: {}", date);
+        LOG.info("Authorization: {}", authorization);
+
+        Credentials credentials = new Credentials("foo", "bar"); // TODO
+
+        boolean valid = AuthorizationUtils.checkAuthorization(
+                authorization,
+                credentials,
+                request.getMethod(),
+                contentMD5,
+                contentType,
+                date,
+                resource);
+
+        return new AuthorizationContext(credentials.getAccessKey(), valid);
     }
 
 }
