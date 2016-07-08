d6f42a6356d9db055a995999b0b7e296cd97bdf9
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-08 17:41:55 +0200

    implemented random generation of access and secret key

--- src/main/java/de/jeha/s3srv/common/security/Credentials.java
+++ src/main/java/de/jeha/s3srv/common/security/Credentials.java
@@ -1,9 +1,17 @@
 package de.jeha.s3srv.common.security;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 /**
  * @author jenshadlich@googlemail.com
  */
 public class Credentials {
+
+    private final static String ACCESS_KEY_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final static int ACCESS_KEY_LENGTH = 20;
+
+    private final static String SECRET_KEY_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/+";
+    private final static int SECRET_KEY_LENGTH = 40;
 
     private final String accessKey;
     private final String secretKey;
@@ -19,6 +27,13 @@ public class Credentials {
 
     public String getSecretKey() {
         return secretKey;
+    }
+
+    public static Credentials generateRandom() {
+        String accessKey = RandomStringUtils.random(ACCESS_KEY_LENGTH, ACCESS_KEY_CHARS);
+        String secretKey = RandomStringUtils.random(SECRET_KEY_LENGTH, SECRET_KEY_CHARS);
+
+        return new Credentials(accessKey, secretKey);
     }
 
 }
