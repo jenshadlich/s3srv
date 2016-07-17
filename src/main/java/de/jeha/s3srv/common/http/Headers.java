1f4f6dbbcd971590868d1e1097e31328d3c6e535
Jens Hadlich <jenshadlich@googlemail.com>
2016-07-17 21:06:35 +0200

    add header constants

---
+++ src/main/java/de/jeha/s3srv/common/http/Headers.java
@@ -0,0 +1,16 @@
+package de.jeha.s3srv.common.http;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Headers {
+
+    public static String AUTHORIZATION = "Authorization";
+    public static String CONNECTION = "Connection";
+    public static String CONTENT_MD5 = "Content-MD5";
+    public static String CONTENT_LENGTH = "Content-Length";
+    public static String CONTENT_TYPE = "Content-Type";
+    public static String DATE = "Date";
+    public static String LOCATION = "Location";
+
+}
