bafae5bff6cf3b0bdc19c1dc22d80cf32da8eb96
Jens Hadlich <jenshadlich@googlemail.com>
2016-09-23 16:04:39 +0200

    add unmarshaller

--- src/main/java/de/jeha/s3srv/xml/JaxbMarshaller.java
+++ src/main/java/de/jeha/s3srv/xml/JaxbMarshaller.java
@@ -2,7 +2,10 @@ package de.jeha.s3srv.xml;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 /**
@@ -11,6 +14,7 @@ import java.io.StringWriter;
 public class JaxbMarshaller {
 
     private static Marshaller MARSHALLER = null;
+    private static Unmarshaller UNMARSHALLER = null;
 
     static {
         try {
@@ -19,6 +23,11 @@ public class JaxbMarshaller {
             MARSHALLER.setProperty(Marshaller.JAXB_FRAGMENT, true);
         } catch (JAXBException e) {
             throw new RuntimeException("Unable to initialize JAXB Marshaller", e);
+        }
+        try {
+            UNMARSHALLER = JaxbContextHolder.getContext().createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new RuntimeException("Unable to initialize JAXB Unmarshaller", e);
         }
     }
 
@@ -32,4 +41,9 @@ public class JaxbMarshaller {
 
         return stringWriter.toString();
     }
+
+    public static <T> T unmarshall(String xml, Class<T> clazz) throws JAXBException, IOException {
+        return UNMARSHALLER.unmarshal(new StreamSource(new StringReader(xml)), clazz).getValue();
+    }
+
 }
