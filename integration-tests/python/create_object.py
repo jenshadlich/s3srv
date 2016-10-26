25c0f5dc03bdc575d5e35f86aaa169cc15648256
Jens Hadlich <jenshadlich@googlemail.com>
2016-10-26 17:47:02 +0200

    change port

--- integration-tests/python/create_object.py
+++ integration-tests/python/create_object.py
@@ -1,14 +1,14 @@
-import boto
 import boto.s3.connection
+
 access_key = 'GUSCQ627K6CC4OAL4RGC'
 secret_key = 'eU8cXNzdXGEFpMMa4SSINjlIeD1fea4pMO3dIw9T'
 conn = boto.connect_s3(
-    aws_access_key_id = access_key,
-    aws_secret_access_key = secret_key,
-    host = 'localhost',
-    port = 8888,
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    host='localhost',
+    port=8080,
     is_secure=False,
-    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
 )
 
 bucket = conn.get_bucket('test-bucket')
--- integration-tests/python/list_buckets.py
+++ integration-tests/python/list_buckets.py
@@ -1,15 +1,15 @@
-import boto
 import boto.s3.connection
+
 access_key = 'GUSCQ627K6CC4OAL4RGC'
 secret_key = 'eU8cXNzdXGEFpMMa4SSINjlIeD1fea4pMO3dIw9T'
 conn = boto.connect_s3(
-    aws_access_key_id = access_key,
-    aws_secret_access_key = secret_key,
-    host = 'localhost',
-    port = 8888,
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    host='localhost',
+    port=8080,
     is_secure=False,
-    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
 )
 
 for bucket in conn.get_all_buckets():
-    print "{name}\t{created}".format(name = bucket.name, created = bucket.creation_date)
+    print "{name}\t{created}".format(name=bucket.name, created=bucket.creation_date)
